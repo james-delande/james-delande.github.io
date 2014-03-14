16f81bab073e1d1561b9cf6273e707582d6db00c
James Delande <james.delande@live.com>
2014-03-14 13:17:56 -0400

    Working AUVish prototype with forward facing and side scan sonar. Switches between the two at end of transitions. Got rid of that horrible yellow background color.

--- myscript.js
+++ myscript.js
@@ -14,16 +14,18 @@ var svgContainer = d3.select("body")
 			.attr("width",w)
 			.attr("height",h);
 			
-var mx = xscale(750);
-var my = yscale(1000);
-function getPath()
-{
- return "M"+mx+","+my+" v-150 a150,150 0 0,0 -150,150 z"
-};
-var lineData = [ { "x": 0,   "y": 500},  { "x": 500,  "y": 500},
-                 { "x": 0,  "y": 250}, { "x": 500,  "y": 250}];
+d3.selectAll("canvas")
+    .attr("width", w)
+    .attr("height", h);
+	
+var canvas = document.getElementById('heatmap');
+heatmap = canvas.getContext('2d');
+var sonarRange = 100, sonarAngle = Math.PI/4, switchSonar = true;
 
-//This is the accessor function we talked about above
+var lineData = [ { "x": 0,   "y": 500},  { "x": 200,  "y": 500},
+                 { "x": 0,  "y": 700}, { "x": 200,  "y": 700}];
+
+//Create the path line
 var lineFunction = d3.svg.line()
                          .x(function(d) { return xscale(d.x); })
                          .y(function(d) { return yscale(d.y); })
@@ -33,46 +35,100 @@ var lineFunction = d3.svg.line()
 //The line SVG Path we draw
 var path = svgContainer.append("path")
                             .attr("d", lineFunction(lineData))
-							.style("stroke-dasharray", ("3,3"))
-                            .attr("stroke", "blue")
+							.style("stroke-dasharray", ("3,3"))//dashed line
+                            .attr("stroke", "green")
                             .attr("stroke-width", 2)
                             .attr("fill", "none");
 
-//Puts circles at the points in the path							
-svgContainer.selectAll(".point")
-    .data(lineData)
-  .enter().append("circle")
-    .attr("r", 2)
-	.attr("fill","red")
-    .attr("transform", function(d) { return "translate(" + [xscale(d.x), yscale(d.y)] + ")"; });
+
 	
-//Circle object, replacing with triangle	
+//Circle object, might be replacing with triangle	
 var circle = svgContainer
 					.append("circle")
 					.attr("r", 5)
 					.attr("transform","translate("+[xscale(lineData[0].x), yscale(lineData[0].y)] + ")");
-					
-var triangle = svgContainer
-					.append("path")
-					.attr("d", "M"+mx+","+my+"l-3,-8 l-3,8")
-					.attr("stroke","red")
-					.attr("fill","red");
+
+
 transition();
 
 function transition() {
   circle.transition()
       .duration(10000)
+	  .ease("linear")
       .attrTween("transform", translateAlong(path.node()))
       .each("end", transition);
-};
+  switchSonar = !switchSonar;
+}
 
 // Returns an attrTween for translating along the specified path element.
 function translateAlong(path) {
   var l = path.getTotalLength();
   return function(d, i, a) {
     return function(t) {
-      var p = path.getPointAtLength(t * l);
-      return "translate(" + p.x + "," + p.y + ")";
+		var p = path.getPointAtLength(t * l),p1;
+		if(t-.0001 < 0){
+			p1 = path.getPointAtLength(0);
+		}else{
+			p1 = path.getPointAtLength((t-.0001) * l);
+		}
+		if(switchSonar){
+			drawForwardSonar(p,p1);
+		}else{
+			drawSideScanSonar(p,p1);
+		}
+		return "translate(" + p.x + "," + p.y + ")";
     };
   };
 };
+
+function drawForwardSonar(p,p1){
+		heatmap.clearRect(0,0,canvas.width,canvas.height);
+		var heading = Math.atan2((p.y-p1.y),(p.x-p1.x));//heading in radians, 0 is 3 O'Clock
+		if(isNaN(heading)){//we are going vertically, i.e. p1.x == p.x
+			if(p.y>p1.y){
+				heading = Math.PI/2;
+			}else{
+				heading = -Math.PI/2;
+			}
+			console.log(heading);
+		}
+
+		var center = [p.x+sonarRange * Math.cos(heading), p.y+sonarRange * Math.sin(heading)];
+		//console.log(heading + " " + Math.sqrt(Math.pow(p.x-center[0],2) + Math.pow(p.y-center[1],2)));
+		heatmap.beginPath();
+		
+		heatmap.lineTo(center[0]+sonarRange*Math.cos(heading-sonarAngle),center[1]+sonarRange*Math.sin(heading-sonarAngle));
+		heatmap.moveTo(p.x,p.y);
+		heatmap.arc(center[0],center[1], sonarRange/2, heading-sonarAngle, heading+sonarAngle, false);
+		heatmap.fill();
+		heatmap.closePath();
+
+		
+};
+
+function drawSideScanSonar(p,p1){
+		heatmap.clearRect(0,0,canvas.width,canvas.height);
+		var heading = Math.atan2((p.y-p1.y),(p.x-p1.x));//heading in radians, 0 is 3 O'Clock
+		if(isNaN(heading)){//we are going vertically, i.e. p1.x == p.x
+			if(p.y>p1.y){
+				heading = Math.PI/2;
+			}else{
+				heading = -Math.PI/2;
+			}
+			console.log(heading);
+		}
+		var perp = [heading+Math.PI/2, heading-Math.PI/2];
+		var center = [p.x+sonarRange * Math.cos(perp[0]), p.y+sonarRange * Math.sin(perp[0]),
+					p.x+sonarRange * Math.cos(perp[1]), p.y+sonarRange * Math.sin(perp[1])];
+		heatmap.beginPath();		
+		heatmap.lineTo(center[0]+sonarRange*Math.cos(perp[0]-sonarAngle),center[1]+sonarRange*Math.sin(perp[0]-sonarAngle));
+		heatmap.moveTo(p.x,p.y);
+		heatmap.arc(center[0],center[1], sonarRange/2, perp[0]-sonarAngle, perp[0]+sonarAngle, false);
+		heatmap.moveTo(p.x,p.y);
+		heatmap.lineTo(center[2]+sonarRange*Math.cos(perp[1]-sonarAngle),center[3]+sonarRange*Math.sin(perp[1]-sonarAngle));
+		heatmap.moveTo(p.x,p.y);
+		heatmap.arc(center[2],center[3], sonarRange/2, perp[1]-sonarAngle, perp[1]+sonarAngle, false);
+		heatmap.fill();
+		heatmap.closePath();		
+};
+

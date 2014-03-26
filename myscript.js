5b59593f04e700c07419a72f8e525a7bccde291d
James Delande <james.delande@live.com>
2014-03-26 14:48:11 -0400

    Added in dual end slider.
    Only works on beginning edge, need to change the drawing to work over a
    scale. Needs stylizing.

--- myscript.js
+++ myscript.js
@@ -77,7 +77,7 @@ for(i=0;i<lineData.length;i++){
 					.attr("transform","translate("+[xscale(lineData[i][0].x), yscale(lineData[i][0].y)] + ")"));
 }
 
-transition();
+//transition();
 
 
 function transition() {
@@ -348,6 +348,109 @@ function drawInTime(time){
 };
 //Slider using Brush
 //Modified from http://bl.ocks.org/mbostock/6452972
+// var width = 500,
+    // height = 100;
+		
+// var x = d3.scale.linear()
+    // .domain([0, 100])
+    // .range([25, 475])
+    // .clamp(true);
+
+// // var brush = d3.svg.brush()
+    // // .x(x)
+    // // .extent([0, 0])
+    // // .on("brush", brushed);
+	
+// var arc = d3.svg.arc()
+    // .outerRadius(height / 2)
+    // .startAngle(0)
+    // .endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
+	
+// var brush = d3.svg.brush()
+    // .x(x)
+    // .extent([.3, .5])
+    // .on("brushstart", brushstart)
+    // .on("brush", brushmove)
+    // .on("brushend", brushend);
+
+// var brushg = svg.append("g")
+    // .attr("class", "brush")
+    // .call(brush);
+
+// brushg.selectAll(".resize").append("path")
+    // .attr("transform", "translate(0," +  height / 2 + ")")
+    // .attr("d", arc);
+
+// brushg.selectAll("rect")
+    // .attr("height", height);
+
+// brushstart();
+// brushmove();
+
+// function brushstart() {
+  // svg.classed("selecting", true);
+// }
+
+// function brushmove() {
+  // var s = brush.extent();
+  // circle.classed("selected", function(d) { return s[0] <= d && d <= s[1]; });
+// }
+
+// function brushend() {
+  // svg.classed("selecting", !d3.event.target.empty());
+// }
+// var sliderSvg = d3.select("body").append("svg")
+    // .attr("width", width)
+    // .attr("height", height)
+	// .attr("class", "sliderSVG");
+
+// sliderSvg.append("g")
+    // .attr("class", "x axis")
+    // .attr("transform", "translate(0," + height / 2 + ")")
+    // .call(d3.svg.axis()
+      // .scale(x)
+      // .orient("bottom")
+      // .tickFormat(function(d) { return d + "%"; })
+      // .tickSize(0)
+      // .tickPadding(12))
+  // .select(".domain")
+  // .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    // .attr("class", "halo");
+
+// var slider = sliderSvg.append("g")
+    // .attr("class", "slider")
+    // .call(brush);
+
+// slider.selectAll(".extent,.resize")
+    // .remove();
+
+// slider.select(".background")
+    // .attr("height", height)
+	// .attr("x",25);
+
+// var handle = slider.append("circle")
+    // .attr("class", "handle")
+    // .attr("transform", "translate(0," + height / 2 + ")")
+    // .attr("r", 9)
+	// .attr("cx",25);
+
+
+// function brushed() {
+	// var value = brush.extent()[0];
+	// if (d3.event.sourceEvent) { // not a programmatic event
+		// value = x.invert(d3.mouse(this)[0]);
+		// brush.extent([value, value]);
+	// }
+	// if(last>=2){
+		// d3.select(".legendSVG").remove();
+		// last = 0;
+		// image = heatmap3.createImageData(canvas2.width,canvas2.height); 
+	// }
+	// handle.attr("cx", x(value));
+	// drawInTime(value);
+// }
+var data = d3.range(800).map(Math.random);
+
 var width = 500,
     height = 100;
 
@@ -358,9 +461,16 @@ var x = d3.scale.linear()
 
 var brush = d3.svg.brush()
     .x(x)
-    .extent([0, 0])
-    .on("brush", brushed);
+    .extent([40, 50])
+    .on("brushstart", brushstart)
+    .on("brush", brushmove)
+    .on("brushend", brushend);
 
+var arc = d3.svg.arc()
+    .outerRadius(height / 2 - 10)
+    .startAngle(0)
+    .endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
+	
 var sliderSvg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
@@ -368,46 +478,44 @@ var sliderSvg = d3.select("body").append("svg")
 
 sliderSvg.append("g")
     .attr("class", "x axis")
-    .attr("transform", "translate(0," + height / 2 + ")")
+    .attr("transform", "translate(0," + (height-15) + ")")
+	.attr("y",height)
     .call(d3.svg.axis()
       .scale(x)
       .orient("bottom")
       .tickFormat(function(d) { return d + "%"; })
-      .tickSize(0)
-      .tickPadding(12))
+      .tickSize(-height +20)
+      .tickPadding(8))
   .select(".domain")
   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
     .attr("class", "halo");
 
-var slider = sliderSvg.append("g")
-    .attr("class", "slider")
+
+var brushg = sliderSvg.append("g")
+    .attr("class", "brush")
     .call(brush);
 
-slider.selectAll(".extent,.resize")
-    .remove();
+brushg.selectAll(".resize").append("path")
+    .attr("transform", "translate(0," +  (height / 2-10) + ")")
+    .attr("d", arc);
 
-slider.select(".background")
-    .attr("height", height)
-	.attr("x",25);
+brushg.selectAll("rect")
+    .attr("height", height-20);
+	
+brushstart();
+brushmove();
 
-var handle = slider.append("circle")
-    .attr("class", "handle")
-    .attr("transform", "translate(0," + height / 2 + ")")
-    .attr("r", 9)
-	.attr("cx",25);
+function brushstart() {
+  sliderSvg.classed("selecting", true);
+}
 
+function brushmove() {
+  var s = brush.extent();
+  	//console.log(s);
+	drawInTime(s[0]);
+  //circle.classed("selected", function(d) { return s[0] <= d && d <= s[1]; });  
+}
 
-function brushed() {
-	var value = brush.extent()[0];
-	if (d3.event.sourceEvent) { // not a programmatic event
-		value = x.invert(d3.mouse(this)[0]);
-		brush.extent([value, value]);
-	}
-	if(last>=2){
-		d3.select(".legendSVG").remove();
-		last = 0;
-		image = heatmap3.createImageData(canvas2.width,canvas2.height); 
-	}
-	handle.attr("cx", x(value));
-	drawInTime(value);
+function brushend() {
+  sliderSvg.classed("selecting", !d3.event.target.empty());
 }

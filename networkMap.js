c94986861c92bb2d92b098948689950530999240
James Delande <james.delande@live.com>
2014-05-06 15:39:22 -0400

    Updated network map to have mouseover features.
    Mousing over an element shows the connection, mousing over a vehicle
    number highlights the vehicle path.

--- networkMap.js
+++ networkMap.js
@@ -1,5 +1,5 @@
-var nw = 200,
-    nh = 200;
+var nw = 142,
+    nh = 142;
 	
 var networkMap = new Array();
 //networkMap.push([0,1,2,3,4,5,6]);
@@ -20,13 +20,58 @@ function inRange(v1, v2){
 	}else{
 		return false;
 	}
-}
+};
 
 function redrawGrid(){
 	d3.selectAll(".networkSVG").remove();
 	drawGrid();
 };
 
+function drawConnection(v1, v2){
+	var v1x = parseFloat(d3.selectAll(".UUV"+v1).attr("cx")),
+						v1y = parseFloat(d3.selectAll(".UUV"+v1).attr("cy"));
+	var v2x = parseFloat(d3.selectAll(".UUV"+v2).attr("cx")),
+						v2y = parseFloat(d3.selectAll(".UUV"+v2).attr("cy"));
+	var dist = Math.sqrt(Math.pow(v1x-v2x,2)+Math.pow(v1y-v2y,2));
+	var conn = svgContainer.append("line")
+			.attr("class", "connection")
+			.attr("x1", v1x)
+			.attr("y1", v1y)
+			.attr("x2", v2x)
+			.attr("y2", v2y)
+			.attr("stroke-width", 2);
+			
+	if(dist <= sonarRange){
+		conn.style("stroke", "green");
+	}else{
+		conn.style("stroke", "red")
+			.style("stroke-dasharray", ("3,2,3"));
+	}
+};
+function highlight(veh){
+var node = d3.select(".path"+veh).node();
+var bbox = node.getBBox();
+var xBox = Math.floor(bbox.x + bbox.width/2.0);
+var yBox = Math.floor(bbox.y + bbox.height/2.0);
+svgContainer.append("rect")
+			.attr("class", "highlight")
+			.attr("x",bbox.x)
+			.attr("y",bbox.y)
+			.attr("width",bbox.width)
+			.attr("height",bbox.height)
+			.attr("fill", "none")
+			.attr("stroke","black")
+			.transition()
+			.duration(2000)
+			.ease("linear")
+			.attr("transform", "translate("+[-xBox*.5, -yBox*.5]+") scale(1.5)")
+			.each("end", function(){
+					d3.select(this).transition()
+					.duration(2000)
+					.ease("linear")
+					.attr("transform", "translate("+[-xBox*.1, -yBox*.1]+") scale(1.1)");
+					});
+};
 function drawGrid(){
 
 	var networkSvg = d3.select("body").append("svg")
@@ -43,21 +88,30 @@ function drawGrid(){
 	var col = row.selectAll(".cell")
 				 .data(function (d) { return d; })
 				.enter().append("rect")
-				 .attr("class", function(d,i){return "cell"+i;})
+				 .attr("class", function(d,i){return "cell cell"+i;})
 				 .attr("x", function(d,i) { return i*20; })
 				 .attr("y", function(d,i) { return d*20; })
 				 .attr("width", function(d) { return 20; })
 				 .attr("height", function(d) { return 20; })
-				 .on('mouseover', function() {
-					// d3.select(this)
-                        // .style('fill', '#FFF');
+				 .on('mouseover', function(d,i) {
+					if(d===i){
+						//do nothing
+					}else if(d===0){
+						highlight(i-1);
+					}else if(i===0){
+						highlight(d-1);
+					}else{
+						drawConnection(d-1,i-1);
+					}
 				 })
-				 .on('mouseout', function() {
-					// d3.select(this)
-                        // .style('fill', '#FFF');
-				 })
-				 .on('click', function() {
-					console.log(d3.select(this));
+				 .on('mouseout', function(d,i) {
+					if(d===i){
+						//do nothing
+					}else if(d===0 || i ===0){
+						d3.selectAll(".highlight").remove();
+					}else{
+						d3.selectAll(".connection").remove();
+					}
 				 })
 				 .style("fill", function(d,i){
 						if(d===i){
@@ -80,10 +134,10 @@ function drawGrid(){
 						}
 					});
 					
-	var labels = row.selectAll(".cell")
+	var labels = row.selectAll(".label")
 					 .data(function (d) { return d; })
 					.enter().append("text")
-					 .attr("class", function(d,i){return "label"+i;})
+					 .attr("class", function(d,i){return "label label"+i;})
 					 .attr("x", function(d,i) { return i*20+5; })
 					 .attr("y", function(d,i) { return d*20+15; })
 					 .text(function(d,i){
@@ -100,5 +154,19 @@ function drawGrid(){
 						}else{
 							return "\u2717";
 						}
-					});
+					})				 
+					.on('mouseover', function(d,i) {
+						if((d===0 || i ===0)||(d===i)){
+							//do nothing
+						}else{
+							drawConnection(d-1,i-1);
+						}
+					 })
+					 .on('mouseout', function(d,i) {
+						if((d===0 || i ===0)||(d===i)){
+							//do nothing
+						}else{
+							d3.selectAll(".connection").remove();
+						}
+					 });
 };
